ffefa2c3d47d10a78c5e248adc02708d35efb011
GHTK\huynt2 <huynt93@ghtk.co>
2024-02-15 15:05:35 +0700

    refactor

--- src/OptimizedMySqlConnector.php
+++ src/OptimizedMySqlConnector.php
@@ -10,27 +10,18 @@ class OptimizedMySqlConnector extends MySqlConnector
     public function connect(array $config)
     {
         $dsn = $this->getDsn($config);
+
         $options = $this->getOptions($config);
+
         // We need to grab the PDO options that should be used while making the brand
         // new connection instance. The PDO options control various aspects of the
         // connection's behavior, and some might be specified by the developers.
         $connection = $this->createConnection($dsn, $config, $options);
 
-        if (!empty($config['database'])) {
-            $connection->exec("use `{$config['database']}`;");
+        if (! empty($config['database'])) {
             $connection->exec("USE `{$config['database']}`;");
         }
 
-        $this->configureIsolationLevel($connection, $config);
-
-        $this->configureEncoding($connection, $config);
-
-        // Next, we will check to see if a timezone has been specified in this config
-        // and if it has we will issue a statement to modify the timezone with the
-        // database. Setting this DB timezone is an optional configuration item.
-        $this->configureTimezone($connection, $config);
-
-        $this->setModes($connection, $config);
         $this->configureConnection($connection, $config);
 
         return $connection;
